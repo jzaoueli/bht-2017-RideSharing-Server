01b7f84b4d0e33b0044fdf65b0c995dd2d2eebf9
jihed zaoueli <s58895@beuth-hochschule.de>
2017-05-16 16:57:38 +0200

    add package general with common and dataaccess
    - to adjust and delete unusable classes

---
+++ ridesharing/core/src/main/java/io/oasp/logistic/ridesharing/general/common/api/RestService.java
@@ -0,0 +1,9 @@
+package io.oasp.logistic.ridesharing.general.common.api;
+
+/**
+ * A marker interface for REST services. REST service interfaces of components should extend this interface.
+ *
+ */
+public interface RestService {
+
+}
