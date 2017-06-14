91f4a44b90df8827108c41485676c201d16ea360
jzaoueli <zaoueli.jihed@gmail.com>
2017-06-14 18:39:50 +0200

    delete ridesharing and samples module with pom.xml

--- ridesharing/core/src/main/java/io/oasp/logistic/ridesharing/usermanagement/dataaccess/api/dao/CGUserProfileDao.java
+++
@@ -1,8 +0,0 @@
-package io.oasp.logistic.ridesharing.usermanagement.dataaccess.api.dao;
-
-import io.oasp.logistic.ridesharing.general.dataaccess.api.dao.ApplicationDao;
-import io.oasp.module.jpa.dataaccess.api.MasterDataDao;
-import io.oasp.logistic.ridesharing.usermanagement.dataaccess.api.CGUserProfileEntity;
-
-public interface CGUserProfileDao extends ApplicationDao<CGUserProfileEntity>, MasterDataDao<CGUserProfileEntity> {
-}
