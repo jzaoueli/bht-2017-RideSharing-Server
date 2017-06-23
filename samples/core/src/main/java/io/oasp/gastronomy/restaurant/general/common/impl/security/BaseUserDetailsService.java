30d36d00a05a2f66f714c6c05d764a10d7bde394
jihed zaoueli <s58895@beuth-hochschule.de>
2017-06-23 13:16:25 +0200

    add security classes to require login/token with request

--- samples/core/src/main/java/io/oasp/gastronomy/restaurant/general/common/impl/security/BaseUserDetailsService.java
+++ samples/core/src/main/java/io/oasp/gastronomy/restaurant/general/common/impl/security/BaseUserDetailsService.java
@@ -24,6 +24,7 @@ import io.oasp.module.security.common.api.accesscontrol.AccessControl;
 import io.oasp.module.security.common.api.accesscontrol.AccessControlProvider;
 import io.oasp.module.security.common.api.accesscontrol.PrincipalAccessControlProvider;
 import io.oasp.module.security.common.base.accesscontrol.AccessControlGrantedAuthority;
+import io.oasp.gastronomy.restaurant.general.service.impl.config.BaseWebSecurityConfig;
 
 /**
  * This class represents a customized implementation of the {@link UserDetailsService} interface.<br/>
@@ -36,7 +37,7 @@ import io.oasp.module.security.common.base.accesscontrol.AccessControlGrantedAut
  * <li>Add the {@link BaseUserDetailsService} to the {@code HttpSecurity} object.</li>
  * </ul>
  * The following code snippet shows the above steps:<br/>
- *
+ * <p>
  * <pre>
  * &#64;Configuration
  * &#64;EnableWebSecurity
@@ -51,155 +52,157 @@ import io.oasp.module.security.common.base.accesscontrol.AccessControlGrantedAut
  *   }
  * }
  * </pre>
- *
+ * <p>
  * <br/>
  * For another example, have a look at {@link BaseWebSecurityConfig}.
  */
 @Named
 public class BaseUserDetailsService implements UserDetailsService {
 
-  /** Logger instance. */
-  private static final Logger LOG = LoggerFactory.getLogger(BaseUserDetailsService.class);
+    /**
+     * Logger instance.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(BaseUserDetailsService.class);
 
-  private Usermanagement usermanagement;
+    private Usermanagement usermanagement;
 
-  private AuthenticationManagerBuilder amBuilder;
+    private AuthenticationManagerBuilder amBuilder;
 
-  private AccessControlProvider accessControlProvider;
+    private AccessControlProvider accessControlProvider;
 
-  private PrincipalAccessControlProvider<UserProfile> principalAccessControlProvider;
+    private PrincipalAccessControlProvider<UserProfile> principalAccessControlProvider;
 
-  @Override
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-    UserProfile principal = retrievePrincipal(username);
-    Set<GrantedAuthority> authorities = getAuthorities(principal);
-    UserDetails user;
-    try {
-      // amBuilder uses the InMemoryUserDetailsManager, because it is configured in BaseWebSecurityConfig
-      user = getAmBuilder().getDefaultUserDetailsService().loadUserByUsername(username);
-      UserData userData = new UserData(user.getUsername(), user.getPassword(), authorities);
-      userData.setUserProfile(principal);
-      return userData;
-    } catch (Exception e) {
-      e.printStackTrace();
-      UsernameNotFoundException exception = new UsernameNotFoundException("Authentication failed.", e);
-      LOG.warn("Failed to get user {}.", username, exception);
-      throw exception;
+        UserProfile principal = retrievePrincipal(username);
+        Set<GrantedAuthority> authorities = getAuthorities(principal);
+        UserDetails user;
+        try {
+            // amBuilder uses the InMemoryUserDetailsManager, because it is configured in BaseWebSecurityConfig
+            user = getAmBuilder().getDefaultUserDetailsService().loadUserByUsername(username);
+            UserData userData = new UserData(user.getUsername(), user.getPassword(), authorities);
+            userData.setUserProfile(principal);
+            return userData;
+        } catch (Exception e) {
+            e.printStackTrace();
+            UsernameNotFoundException exception = new UsernameNotFoundException("Authentication failed.", e);
+            LOG.warn("Failed to get user {}.", username, exception);
+            throw exception;
+        }
     }
-  }
 
-  /**
-   * Returns the {@link GrantedAuthority}s of the user associated with the provided {@link UserProfile}.
-   *
-   * @param principal the {@link UserProfile} of the user
-   * @return the associated {@link GrantedAuthority}s
-   * @throws AuthenticationException if no principal is retrievable for the given {@code username}
-   */
-  protected Set<GrantedAuthority> getAuthorities(UserProfile principal) throws AuthenticationException {
+    /**
+     * Returns the {@link GrantedAuthority}s of the user associated with the provided {@link UserProfile}.
+     *
+     * @param principal the {@link UserProfile} of the user
+     * @return the associated {@link GrantedAuthority}s
+     * @throws AuthenticationException if no principal is retrievable for the given {@code username}
+     */
+    protected Set<GrantedAuthority> getAuthorities(UserProfile principal) throws AuthenticationException {
 
-    if (principal == null) {
-      LOG.warn("Principal must not be null.");
-      throw new IllegalArgumentException();
+        if (principal == null) {
+            LOG.warn("Principal must not be null.");
+            throw new IllegalArgumentException();
+        }
+        // determine granted authorities for spring-security...
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        Collection<String> accessControlIds = this.principalAccessControlProvider.getAccessControlIds(principal);
+        Set<AccessControl> accessControlSet = new HashSet<>();
+        for (String id : accessControlIds) {
+            boolean success = this.accessControlProvider.collectAccessControls(id, accessControlSet);
+            if (!success) {
+                LOG.warn("Undefined access control {}.", id);
+            }
+        }
+        for (AccessControl accessControl : accessControlSet) {
+            authorities.add(new AccessControlGrantedAuthority(accessControl));
+        }
+        return authorities;
     }
-    // determine granted authorities for spring-security...
-    Set<GrantedAuthority> authorities = new HashSet<>();
-    Collection<String> accessControlIds = this.principalAccessControlProvider.getAccessControlIds(principal);
-    Set<AccessControl> accessControlSet = new HashSet<>();
-    for (String id : accessControlIds) {
-      boolean success = this.accessControlProvider.collectAccessControls(id, accessControlSet);
-      if (!success) {
-        LOG.warn("Undefined access control {}.", id);
-      }
+
+    /**
+     * @param username The {@code username} for which the {@code UserProfile} will be queried.
+     * @return An instance of type {@link UserProfile} obtained by querying the {@code username}.
+     */
+    protected UserProfile retrievePrincipal(String username) {
+
+        try {
+            return this.usermanagement.findUserProfileByLogin(username);
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            UsernameNotFoundException exception = new UsernameNotFoundException("Authentication failed.", e);
+            LOG.warn("Failed to get user {}.", username, exception);
+            throw exception;
+        }
     }
-    for (AccessControl accessControl : accessControlSet) {
-      authorities.add(new AccessControlGrantedAuthority(accessControl));
+
+    /**
+     * @return usermanagement
+     */
+    public Usermanagement getUsermanagement() {
+
+        return this.usermanagement;
     }
-    return authorities;
-  }
 
-  /**
-   * @param username The {@code username} for which the {@code UserProfile} will be queried.
-   * @return An instance of type {@link UserProfile} obtained by querying the {@code username}.
-   */
-  protected UserProfile retrievePrincipal(String username) {
+    /**
+     * @param usermanagement new value of {@link #getUsermanagement}.
+     */
+    @Inject
+    public void setUsermanagement(Usermanagement usermanagement) {
 
-    try {
-      return this.usermanagement.findUserProfileByLogin(username);
-    } catch (RuntimeException e) {
-      e.printStackTrace();
-      UsernameNotFoundException exception = new UsernameNotFoundException("Authentication failed.", e);
-      LOG.warn("Failed to get user {}.", username, exception);
-      throw exception;
+        this.usermanagement = usermanagement;
     }
-  }
 
-  /**
-   * @return usermanagement
-   */
-  public Usermanagement getUsermanagement() {
+    /**
+     * @return amBuilder
+     */
+    public AuthenticationManagerBuilder getAmBuilder() {
 
-    return this.usermanagement;
-  }
+        return this.amBuilder;
+    }
 
-  /**
-   * @param usermanagement new value of {@link #getUsermanagement}.
-   */
-  @Inject
-  public void setUsermanagement(Usermanagement usermanagement) {
+    /**
+     * @param amBuilder new value of {@link #getAmBuilder}.
+     */
+    @Inject
+    public void setAmBuilder(AuthenticationManagerBuilder amBuilder) {
 
-    this.usermanagement = usermanagement;
-  }
+        this.amBuilder = amBuilder;
+    }
 
-  /**
-   * @return amBuilder
-   */
-  public AuthenticationManagerBuilder getAmBuilder() {
+    /**
+     * @return accessControlProvider
+     */
+    public AccessControlProvider getAccessControlProvider() {
 
-    return this.amBuilder;
-  }
+        return this.accessControlProvider;
+    }
 
-  /**
-   * @param amBuilder new value of {@link #getAmBuilder}.
-   */
-  @Inject
-  public void setAmBuilder(AuthenticationManagerBuilder amBuilder) {
+    /**
+     * @param accessControlProvider new value of {@link #getAccessControlProvider}.
+     */
+    @Inject
+    public void setAccessControlProvider(AccessControlProvider accessControlProvider) {
 
-    this.amBuilder = amBuilder;
-  }
+        this.accessControlProvider = accessControlProvider;
+    }
 
-  /**
-   * @return accessControlProvider
-   */
-  public AccessControlProvider getAccessControlProvider() {
+    /**
+     * @return principalAccessControlProvider
+     */
+    public PrincipalAccessControlProvider<UserProfile> getPrincipalAccessControlProvider() {
 
-    return this.accessControlProvider;
-  }
+        return this.principalAccessControlProvider;
+    }
 
-  /**
-   * @param accessControlProvider new value of {@link #getAccessControlProvider}.
-   */
-  @Inject
-  public void setAccessControlProvider(AccessControlProvider accessControlProvider) {
+    /**
+     * @param principalAccessControlProvider new value of {@link #getPrincipalAccessControlProvider}.
+     */
+    @Inject
+    public void setPrincipalAccessControlProvider(
+            PrincipalAccessControlProvider<UserProfile> principalAccessControlProvider) {
 
-    this.accessControlProvider = accessControlProvider;
-  }
-
-  /**
-   * @return principalAccessControlProvider
-   */
-  public PrincipalAccessControlProvider<UserProfile> getPrincipalAccessControlProvider() {
-
-    return this.principalAccessControlProvider;
-  }
-
-  /**
-   * @param principalAccessControlProvider new value of {@link #getPrincipalAccessControlProvider}.
-   */
-  @Inject
-  public void setPrincipalAccessControlProvider(
-      PrincipalAccessControlProvider<UserProfile> principalAccessControlProvider) {
-
-    this.principalAccessControlProvider = principalAccessControlProvider;
-  }
+        this.principalAccessControlProvider = principalAccessControlProvider;
+    }
 }
